c680aa8e1db6e89696cdee16b3d7c316e33e81f1
Kaworudesu <pedro.arcanjo2002@gmail.com>
2024-05-08 00:46:23 -0300

    update: pilot info beta page added

--- src/pages/Pilot.jsx
+++ src/pages/Pilot.jsx
@@ -16,45 +16,44 @@ function Pilot({ data }) {
   }
 
   return (
-    <div className="max-h-screen min-h-screen bg-slate-100 text-slate-800 font-custom">
-      <h1 className="text-center underline p-2 text-4xl font-semibold ">
+    <div className="h-screen bg-slate-900 text-slate-800">
+      <h1 className="text-left text-slate-100 underline p-2 ml-16 text-4xl font-semibold ">
         {pilot.title}
       </h1>
-      <div className="backgroundImg absolute bottom-0 left-0">
+      <div className="backgroundImg absolute -bottom-10 left-10">
         <img src={pilot.backgroundImage} alt="" height={350} width={350} />
       </div>
-      <div className="textContent w-[60%] float-right p-2 flex flex-col items-center justify-around h-full">
-        <div className="container flex justify-between">
-          <div className="text bg-slate-50 p-2 w-[350px] overflow-auto h-[250px] rounded">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            minus ad libero tenetur suscipit. Illum tenetur quaerat eaque
-            consequatur Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dignissimos minus ad libero tenetur suscipit. Illum tenetur quaerat
-            eaque consequatur Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Dignissimos minus ad libero tenetur suscipit. Illum tenetur
-            quaerat eaque consequaturLorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dignissimos minus ad libero tenetur suscipit.
-            Illum tenetur quaerat eaque consequaturLorem ipsum dolor sit amet
-            consectetur adipisicing elit. Dignissimos minus ad libero tenetur
-            suscipit. Illum tenetur quaerat eaque consequaturLorem ipsum dolor
-            sit amet consectetur adipisicing elit. Dignissimos minus ad libero
-            tenetur suscipit. Illum tenetur quaerat eaque consequaturLorem ipsum
-            dolor sit amet consectetur adipisicing elit. Dignissimos minus ad
-            libero tenetur suscipit. Illum tenetur quaerat eaque consequatur
-            magnam ipsa a velit esse. Possimus veniam inventore aut.
-            Dignissimos, a.
-          </div>
-          <div className="gallery relative w-[420px] h-[220px]">
-            <ImageSlider images={images} interval={4000} />
+      <div className="textContent w-[60%] float-right p-2 grid gap-1 grid-cols-2 grid-rows-2 h-[80%]">
+        <div className="container flex w-full max-h-full row-span-2 justify-between">
+          <div className="text bg-slate-50 p-2  overflow-auto rounded">
+            <h4 className="text-center text-xl font-semibold m-2">About</h4>
+            <span>{pilot.description}
+            </span>
           </div>
         </div>
-        <div className="table border-t-red-50 rounded bottom-2 bg-white p-4 w-[300px]">
+          <div className="gallery relative flex items-center justify-center w-full h-full">
+            <ImageSlider images={images} interval={4000} />
+          </div>
+       
+        <div className=" shadow-md rounded bottom-2 bg-white p-4 w-full flex justify-around">
+          <div>
           <p className="font-semibold">Name: </p>
           <p className="">{pilot.title}</p>
           <p className="font-semibold">Race: </p>
           <p className="">{pilot.race}</p>
+          <p className="font-semibold">Weight: </p>
+          <p className="">{pilot.weight}</p>
+          <p className="font-semibold">Blood Type: </p>
+          <p className="">{pilot.blood}</p>
+          </div>
+          <div> 
+          <p className="font-semibold">Rank: </p>
+          <p className="">{pilot.rank}</p>
+          <p className="font-semibold">Age: </p>
+          <p className="">{pilot.age}</p>
           <p className="font-semibold">Affiliation: </p>
           <p className="">{pilot.affiliation}</p>
+          </div>
         </div>
       </div>
     </div>
